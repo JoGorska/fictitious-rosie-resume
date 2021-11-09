20bcf3ed6ddf838d907aadfad3ac5af18bac193a
Joanna Gorska <gorska919@gmail.com>
2021-11-09 08:56:34 +0000

    Add function userInformationHTML to display users's information

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,4 +1,21 @@
 
+function userInformationHTML(user) {
+    return`
+    <h2>${user.name}
+    <span class="small-name">
+        (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+    </span>
+</h2>
+<div class="gh-content">
+    <div class="gh-avatar">
+        <a href="${user.html_url}" target="_blank">
+            <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+        </a>
+    </div>
+    <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+</div>`;
+}
+
 
 function fetchGitHubInformation(event) {
 
@@ -15,9 +32,11 @@ function fetchGitHubInformation(event) {
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`)
     ).then(
+        // if normal response than displays data for the user
         function(response) {
             var userData = response;
             $("#gh-user-data").html(userInformationHTML(userData));
+        // handles error response within then
         }, function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(`<h2>No Info found for user ${username}</h2>`)
